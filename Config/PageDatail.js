b19f7cccbe991aadf88cfa83036bad3056f8f26f
Peter <47840148+Sweedsiter@users.noreply.github.com>
2024-02-08 07:40:20 +0000

    feat: Add header, update Footer and add Socials

--- Config/PageDatail.js
+++ Config/PageDatail.js
@@ -1,3 +1,4 @@
+// Header
 exports.Header = [
   "Dx-emb WebDesign For You",
   "https://e7.pngegg.com/pngimages/361/627/png-clipart-leaf-logo-green-leaves-green-and-teal-leaf-logo-text-maple-leaf-thumbnail.png",
@@ -7,26 +8,35 @@ exports.Header = [
   "Contact",
   "Addmin",
 ];
-exports.Footer = [
-  { Box1_1: "Company" },
-  {
-    Box1_2:
-      "ซอยเพชรเกษม 106 แขวงหนองค้างพลู เขตหนองแขม กรุงเทพมหานคร 10160 " +
-      "Nong Khang Phlu, Nong Khaem, Bangkok 10160",
-  },
-  { Box2_1: "Home" },
-  { Box2_2: "Product" },
-  { Box2_3: "About" },
-  { Box2_4: "Contact" },
-  { Box2_5: "Addmin" },
-  { Phone: "091-045 5990" },
-  { Email: "Dxemb@dxemb.com" },
-  { Line: "Sweedsiter" },
-  { Facebook: "DxEMB" },
-  { FacebookLink: "https://www.facebook.com/DxEMB" },
-];
-exports.Phone = [{ Phone: "091-045 5990" }];
-exports.Email = [{ Email: "Dxemb@dxemb.com" }];
+
+// Footer
+exports.Footer = {
+  State:
+    "ซอยเพชรเกษม 106 แขวงหนองค้างพลู เขตหนองแขม กรุงเทพมหานคร 10160 " +
+    "Nong Khang Phlu, Nong Khaem, Bangkok 10160",
+  Home: "Home",
+  Product: "Product",
+  About: "About",
+  Contact: "Contact",
+  Addmin: "Addmin",
+
+  Phone: "091-045 5990",
+  Email: "Dxemb@dxemb.com",
+  FacebookLink: "https://www.facebook.com/DxEMB",
+  Facebook: "DxEMB",
+  Line: "Sweedsiter",
+  LineLink: "https://line.me/ti/p/-yPHknooDV",
+  Map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1938.060590982372!2d100.35351809194995!3d13.711109600361308!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e296092f077d85%3A0xdba76fb25af80fb!2z4LiL4Lit4LiiIOC5gOC4nuC4iuC4o-C5gOC4geC4qeC4oSAxMDYg4LmB4LiC4Lin4LiH4Lir4LiZ4Lit4LiH4LiE4LmJ4Liy4LiH4Lie4Lil4Li5IOC5gOC4guC4leC4q-C4meC4reC4h-C5geC4guC4oSDguIHguKPguLjguIfguYDguJfguJ7guKHguKvguLLguJnguITguKMgMTAxNjA!5e0!3m2!1sth!2sth!4v1706510902172!5m2!1sth!2sth",
+};
+
+// Socials
+exports.Socials = {
+  Phone: "091-045 5990",
+  Email: "Dxemb@dxemb.com",
+  FacebookLink: "https://www.facebook.com/DxEMB",
+  Facebook: "DxEMB",
+  Line: "Sweedsiter",
+};
 
 // Pages
 exports.Home = [

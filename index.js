ba0239bf9de186da7fa7b9b6e9e25c1cc9705d6d
Peter <47840148+Sweedsiter@users.noreply.github.com>
2024-03-02 08:57:44 +0000

    02-03

--- index.js
+++ index.js
@@ -12,13 +12,48 @@ app.use(core());
 
 // Sever All
 app.get("/", async function (req, res) {
-  const ad = [{ name: "PeterA" }];
-  const bd = [{ name: "PeterB" }];
+  const ad = [{ name: "PeterA" }, { name: "PeterB" }];
   //   res.json(Product.data);
   res.json(ad);
 });
 app.get("/product", async function (req, res) {
-  res.send(Product.data);
+  //  res.send(Product.data);
+  const Data = [
+    {
+      Group: "Phone",
+      Price: "2555",
+      Image:
+        "https://www.blesssky.com/wp-content/uploads/2023/05/img-product-iPad_10th_Gen_Wi-Fi_Blue-2.jpg",
+      Total: "26",
+      Details: ` กกกกก  Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
+      dolorem quo dignissimos tenet ur reprehenderit. Id, officia.
+      Assumenda numquam molestias a voluptate. Voluptas, possimus
+      temporibus! Amet adipisci odit nihil quod voluptate?`,
+    },
+    {
+      Group: "Phone",
+      Price: "3500",
+      Image:
+        "https://www.tescophoto.com/media/catalog/product/cache/a2112a15e5165072db3b22495c91a3ca/p/h/photoprintfrommobile-tesco.jpg",
+      Total: "26",
+      Details: ` กกกกก  Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
+      dolorem quo dignissimos tenet ur reprehenderit. Id, officia.
+      Assumenda numquam molestias a voluptate. Voluptas, possimus
+      temporibus! Amet adipisci odit nihil quod voluptate?`,
+    },
+    {
+      Group: "Phone",
+      Price: "4000",
+      Image:
+        "https://www.maxphoto.co.uk/media/catalog/product/cache/a2112a15e5165072db3b22495c91a3ca/p/h/photoprintfrommobile-max.jpg",
+      Total: "26",
+      Details: ` กกกกก  Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
+      dolorem quo dignissimos tenet ur reprehenderit. Id, officia.
+      Assumenda numquam molestias a voluptate. Voluptas, possimus
+      temporibus! Amet adipisci odit nihil quod voluptate?`,
+    },
+  ];
+  res.send(Data);
 });
 
 app.listen(port, "localhost", () => {
